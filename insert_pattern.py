17177d57cc14a5b60c088d50f125ad2cae3b97c7
PiotrekJakubowski <35908280+PiotrekJakubowski@users.noreply.github.com>
2019-11-25 21:24:14 +0100

    Add files via upload
    Insert pattern into existing image

---
+++ insert_pattern.py
@@ -0,0 +1,8 @@
+def insert_pattern(background_img, pattern, location):
+    """Inserts a pattern onto a background, at given location. Returns new image."""
+    img = background_img.copy()
+    r0, c0 = location
+    r1, c1 = r0 + pattern.shape[0], c0 + pattern.shape[1]
+    if r1 < background_img.shape[0] and c1 < background_img.shape[1]:
+        img[r0:r1, c0:c1, :] = skimage.img_as_float(pattern)
+    return img
