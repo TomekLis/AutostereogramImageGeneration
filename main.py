72341a087bce3b6423ca7a939213ecaf880c32fe
BrandonSalas1996 <56589961+BrandonSalas1996@users.noreply.github.com>
2020-01-16 06:43:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+# main code of autostereogram project
 from display import display, blank_image 
 from tile_horizontally import tile_horizontally
 from make_pattern import make_pattern
 from depth_map import create_circular_depthmap, normalize
 import numpy as np
 import skimage
-
+# Horizontally Shifted Patterns code
 repeated_image = skimage.io.imread('images/coin.png')
 
 img = blank_image(shape=(450, 800, 4))
@@ -13,7 +14,7 @@ img = tile_horizontally(img, repeated_image, (10 + 150, 10), 5, shift=150)
 img = tile_horizontally(img, repeated_image, (10 + 2*150, 10), 5, shift=140)
 display(img)
 
-
+# Depth map (random dots) code
 pattern = make_pattern(shape=(128, 64))
 
 depthmap = create_circular_depthmap(radius=150)
