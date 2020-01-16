9f17addcb56feb69e80c17eeac127042761d1c40
BrandonSalas1996 <56589961+BrandonSalas1996@users.noreply.github.com>
2020-01-16 06:45:49 +0100

    Update make_pattern.py

--- make_pattern.py
+++ make_pattern.py
@@ -3,3 +3,4 @@ import numpy as np
 def make_pattern(shape=(16, 16), levels=64):
     "Creates a pattern from gray values."
     return np.random.randint(0, levels - 1, shape) / levels
+# For this function we use a random generation of values between 0 and 1, where 1 is white and 0 is black. 
