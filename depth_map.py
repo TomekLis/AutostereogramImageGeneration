2b330d6d0198db4ccff00fc025deccfca81dcc7c
BrandonSalas1996 <56589961+BrandonSalas1996@users.noreply.github.com>
2020-01-16 06:47:35 +0100

    Update depth_map.py

--- depth_map.py
+++ depth_map.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+# Using the general equation of a circle, the depth map is set in black first and then it is compared to the set radius. 
 def create_circular_depthmap(shape=(600, 800), center=None, radius=100):
     "Creates a circular depthmap, centered on the image."
     depthmap = np.zeros(shape, dtype=np.float)
@@ -11,7 +11,7 @@ def create_circular_depthmap(shape=(600, 800), center=None, radius=100):
     d = np.sqrt((R - center[0])**2 + (C - center[1])**2)
     depthmap += (d < radius)
     return depthmap    
-
+# Even if the circle before only has values in a [0,1] range, every depth map inserted must be normalized for having no errors in the application of the algorithm. 
 def normalize(depthmap):
     "Normalizes values of depthmap to [0, 1] range."
     if depthmap.max() > depthmap.min():
